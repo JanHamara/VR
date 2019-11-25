742790d2f467107cfc50510ca15e17688a5073e9
JanHamara <hamara.jan18@gmail.com>
2019-11-25 18:15:44 +0000

    removed unnecessary themes and added custom theme

---
+++ wp-content/themes/vr/index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    VR Theme | Index File
+    Project  | VR for Disabled
+	Copyright: 2019, Jan Hamara, 2184316h@student.gla.ac.uk
+    Wordpress v5.3
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
