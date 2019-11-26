a6ddf69e793a45a58b05c084089c63145ec85852
JanHamara <hamara.jan18@gmail.com>
2019-11-26 15:45:29 +0000

    set up functions php file before setting up webpack and bundling of all stylesheets and scripts in dist directory

---
+++ wp-content/themes/vr/front-page.php
@@ -0,0 +1,16 @@
+<?php
+/*
+    VR Theme | Front Page
+    Project  | VR for Disabled
+	Copyright: 2019, Jan Hamara, 2184316h@student.gla.ac.uk
+    Wordpress v5.3
+*/
+?>
+
+<?php get_header(); ?>
+
+    <main>
+        Hello, this is me.
+    </main>
+
+<?php get_footer(); ?>
