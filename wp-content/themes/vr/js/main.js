699ee61fb6c5494f47f7ba68e698098f7682b3cc
JanHamara <hamara.jan18@gmail.com>
2019-11-26 15:50:58 +0000

    added bootstrap and glyphicons fonts required by bootstrap

---
+++ wp-content/themes/vr/js/main.js
@@ -0,0 +1,16 @@
+// main.js
+
+// Webpack
+require('./bootstrap.min');
+require('../css/bootstrap-theme.min.css');
+require('../css/bootstrap.min.css');
+require('../style.less');
+
+jQuery( document ).ready(function() {
+
+});
+
+jQuery( window ).load(function() {
+
+});
+
