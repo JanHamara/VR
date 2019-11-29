47daab4dad30310427872aa4a29ea0302a01dafe
JanHamara <hamara.jan18@gmail.com>
2019-11-29 16:19:11 +0000

    finished vr menu item 1 with all functionality set up

--- wp-content/themes/vr/template-parts/header/page-accessibility-bar.php
+++ wp-content/themes/vr/template-parts/header/page-accessibility-bar.php
@@ -10,7 +10,7 @@
     <!--    Website Logo - acting as a link to home page    -->
     <a id="vr-site-logo" href="<?php echo get_home_url(); ?>">
         <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png"
-             class="img-responsive" alt="site-logo">
+             class="img-responsive" alt="Website Logo">
     </a>
 
     <!--    Accessibility Button - acts as button to open accessibility settings    -->
